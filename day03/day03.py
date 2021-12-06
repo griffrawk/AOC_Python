ebc0ae87cd628e129adb43fa5006c747c0163cb8
Andy Griffiths <griffiths_andy@icloud.com>
2021-12-06 22:38:12 +0000

    Day 3 part1, with a better method for gamma & epsilon. Also unittest example

--- day03/day03.py
+++ day03/day03.py
@@ -1,35 +1,39 @@
-def part1():
-    ones = {}
-    zeroes = {}
-    gammabits = epsilonbits = ''
-    gamma = epsilon = 0
-    with open('day03_data.txt', 'r', encoding='utf-8') as a_file:
-        for a_line in a_file:
-            a_line = a_line.rstrip()
-            for (pos, bit) in enumerate(a_line):
-                # init dict pos if not in
-                if pos not in ones:
-                    ones[pos] = 0
-                if pos not in zeroes:
-                    zeroes[pos] = 0
-                # inc dict count
-                if bit == '1':
-                    ones[pos] += 1
-                elif bit == '0':
-                    zeroes[pos] += 1
+import unittest
 
-        for (pos, count) in ones.items():
-            if count > zeroes[pos]:
-                gammabits += '1'
-                epsilonbits += '0'
-            else:
-                gammabits += '0'
-                epsilonbits += '1'
-        gamma = int(gammabits, 2)
-        epsilon = int(epsilonbits, 2)
-        print('Power consumption = {}'.format(gamma * epsilon))
-        assert gamma * epsilon == 1997414
+
+class TestMethods(unittest.TestCase):
+
+    def test_part1(self):
+        ones = {}
+        zeroes = {}
+        with open('day03_data.txt', 'r', encoding='utf-8') as a_file:
+            for a_line in a_file:
+                a_line = a_line.rstrip()
+                for (pos, bit) in enumerate(a_line):
+                    # init dict pos if not in
+                    if pos not in ones:
+                        ones[pos] = 0
+                    if pos not in zeroes:
+                        zeroes[pos] = 0
+                    # inc dict count
+                    if bit == '1':
+                        ones[pos] += 1
+                    elif bit == '0':
+                        zeroes[pos] += 1
+
+            gammabits = epsilonbits = ''
+            for (pos, count) in ones.items():
+                if count > zeroes[pos]:
+                    gammabits += '1'
+                    epsilonbits += '0'
+                else:
+                    gammabits += '0'
+                    epsilonbits += '1'
+            gamma = int(gammabits, 2)
+            epsilon = int(epsilonbits, 2)
+            print('Power consumption = {}'.format(gamma * epsilon))
+            self.assertEqual(gamma * epsilon, 1997414)
 
 
 if __name__ == '__main__':
-    part1()
+    unittest.main()
