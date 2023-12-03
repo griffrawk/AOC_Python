0b23a9965901496887d2c64756ec22c63246c381
Andy Griffiths <griffiths_andy@icloud.com>
2023-12-03 00:56:19 +0000

    day1 part1 done

--- aoc2023/day01/test_day01.py
+++ aoc2023/day01/test_day01.py
@@ -2,7 +2,7 @@ from aoc2023.day01.day01 import part_one, part_two
 
 
 def test_part_one():
-    assert part_one() == 1665
+    assert part_one() == 54877
 
 
 def test_part_two():
--- y2021/day02/test_day02.py
+++
@@ -1,6 +0,0 @@
-from day02.day02 import part_one
-
-
-def test_part_one():
-    assert part_one() == 2091984
-
