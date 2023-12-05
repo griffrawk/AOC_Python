29fda0fdfbc6464c98784fad207559910c6ff944
Andy Griffiths <griffiths_andy@icloud.com>
2023-12-05 11:53:09 +0000

    part two combined with part one in a single pass. it foregoes the early loop termination as all data needs to be examined for part 2

--- aoc2023/day02/day02.py
+++ aoc2023/day02/day02.py
@@ -1,5 +1,6 @@
 # day02.py
 import os
+from math import prod
 
 
 class Continue(Exception):
@@ -38,9 +39,10 @@ def part_one_alt():
 # On reflection I like it this way, for this use at least! The game check inner func can be
 # treated as a condition. Clearer to see what it's doing.
 # https://stackoverflow.com/questions/653509/breaking-out-of-nested-loops
+# https://stackoverflow.com/questions/189645/how-can-i-break-out-of-multiple-loops
 def part_one():
     limits = {'red': 12, 'green': 13, 'blue': 14}
-    sum_up = 0
+    sum_up = sum_powers = 0
     with open(os.path.join(os.path.dirname(__file__), 'day02_data.txt'), 'r', encoding='utf-8') as a_file:
         for a_line in a_file:
             game, hands = a_line.split(':')
@@ -61,21 +63,29 @@ def part_one():
     print(sum_up)
     return sum_up
 
-# def part_two():
-#     sumup = 0
-#     numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-#     with open(os.path.join(os.path.dirname(__file__), 'day01_data.txt'), 'r', encoding='utf-8') as a_file:
-#         for a_line in a_file:
-#             # replace the word with corresponding number, but sandwich number in words
-#             # this is to catch cases like 'oneight' where one word flows into another
-#             # so produces 'one1oneight8eight'. this should catch all cases regardless of number replace order
-#             # work on a copy just in case
-#             a_copy = copy.copy(a_line)
-#             for n in range(10):
-#                 a_copy = a_copy.replace(numbers[n], numbers[n] + str(n) + numbers[n])
-#             # print(a_copy)
-#             digits = ''.join(filter(str.isdigit, a_copy))
-#             if len(digits) > 0:
-#                 sumup += int(digits[0:1] + digits[-1])
-#     print(sumup)
-#     return sumup
+
+# Now find the sum of the powers of the maximum of each cube colour count. Combined it with the part_one
+# solution, but now the loop examines every hand in every game. No early loop termination, but check game
+# validity with a flag.
+def part_one_two():
+    sum_up = sum_maxima = 0
+    limits = {'red': 12, 'green': 13, 'blue': 14}
+    with open(os.path.join(os.path.dirname(__file__), 'day02_data.txt'), 'r', encoding='utf-8') as a_file:
+        for a_line in a_file:
+            game, hands = a_line.split(':')
+            maxima = {}
+            valid_game = True
+            for hand in hands.split(';'):
+                for cube in hand.split(','):
+                    count, colour = cube.split()
+                    if int(count) > limits[colour]:
+                        valid_game = False
+                    maxima[colour] = max(maxima.get(colour, 0), int(count))
+
+            if valid_game:
+                sum_up += int(game.split()[1])
+
+            sum_maxima += prod(maxima.values())
+
+    print(sum_up, sum_maxima)
+    return sum_up, sum_maxima
--- aoc2023/day02/test_day02.py
+++ aoc2023/day02/test_day02.py
@@ -1,9 +1,9 @@
-from aoc2023.day02.day02 import part_one
+from aoc2023.day02.day02 import part_one, part_one_two
 
 
 def test_part_one():
     assert part_one() == 2105
 
 
-# def test_part_two():
-#     assert part_two() == 54100
+def test_part_one_two():
+    assert part_one_two() == (2105, 72422)
