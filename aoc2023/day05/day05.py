4d134d994887a6cf0fab17deb1e094986921dbd8
Andy Griffiths <griffiths_andy@icloud.com>
2023-12-17 01:00:13 +0000

    refactor the map storage into tuple

--- aoc2023/day05/day05.py
+++ aoc2023/day05/day05.py
@@ -8,8 +8,8 @@ def part_one():
     current_map = ''
     maps = {}
     valid_maps = ['seed-to-soil map:', 'soil-to-fertilizer map:', 'fertilizer-to-water map:',
-            'water-to-light map:', 'light-to-temperature map:', 'temperature-to-humidity map:',
-            'humidity-to-location map:']
+                  'water-to-light map:', 'light-to-temperature map:', 'temperature-to-humidity map:',
+                  'humidity-to-location map:']
 
     # Parse input into a list of seeds and a collection of seed 'maps'
     with (open(os.path.join(os.path.dirname(__file__), 'day05_data.txt'), 'r', encoding='utf-8') as a_file):
@@ -17,14 +17,14 @@ def part_one():
             a_line = a_line.strip()
             # Find seeds separately all on one line
             if (seed_line := a_line.split(':'))[0] == 'seeds':
-                seeds = re.findall(r'\d+', seed_line[1])
+                seeds = [int(x) for x in re.findall(r'\d+', seed_line[1])]
                 continue
             # Note that the map has changed
             if a_line in valid_maps:
                 current_map = a_line
                 continue
             # Add each of the map lines to a dict of list of tuples
-            if len(a_map := tuple([int(x) for x in re.findall(r'\d+',a_line)])):
+            if len(a_map := tuple([int(x) for x in re.findall(r'\d+', a_line)])):
                 m = maps.get(current_map, [])
                 m.append(a_map)
                 maps[current_map] = m
@@ -33,7 +33,7 @@ def part_one():
         # Pass each seed through all the maps until a location is found. Find min location.
         min_location = float('inf')
         for seed in seeds:
-            next_stage = int(seed)
+            next_stage = seed
             for current_map in valid_maps:
                 for dst, src, rgl in maps[current_map]:
                     if src <= next_stage <= src + rgl:
