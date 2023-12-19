ac4bf8ec5d7ae4e72d6fc7494e8edcda86669087
Andy Griffiths <griffiths_andy@icloud.com>
2023-12-19 00:23:17 +0000

    Day 6 done

--- aoc2023/day06/test_day06.py
+++ aoc2023/day06/test_day06.py
@@ -1,9 +1,23 @@
+
 def test_part_one():
-    race_length = 7
-    race_record = 9
-
+    # races = [(7, 9), (15, 40), (30, 200)]
+    # races = [(47, 400), (98, 1213), (66, 1011), (98, 1540)]
+    races = [(47986698, 400121310111540)]
     speed_increment = 1
+    margin = 1
 
-    for duration in range(0, race_length + 1):
-        distance = duration * speed_increment * (race_length - duration)
-        print('duration', duration, 'travels distance', distance)
+    for race_length, race_record in races:
+        ways_to_win = 0
+        for duration in range(0, race_length + 1):
+            distance = duration * speed_increment * (race_length - duration)
+            if distance >= race_record:
+                ways_to_win += 1
+            # Stop looping if distance has peaked and dropped below record
+            if distance < race_record and ways_to_win > 1:
+                break
+        margin = margin * ways_to_win
+        print('ways', ways_to_win)
+    
+    print('margin', margin)
+    # assert margin == 1660968
+    assert margin == 26499773
