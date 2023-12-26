aa2e6c0f4061a4efec3be55f45a49759cca081df
Andy Griffiths <griffiths_andy@icloud.com>
2023-12-26 11:36:13 +0000

    7.2 pushed though incorrect

--- aoc2023/day07/day07.py
+++ aoc2023/day07/day07.py
@@ -1,59 +1,85 @@
 import os
 import itertools
-
-
-# Provide a sort key for a card hand based on card letters representing values
-# for the ten, court cards and aces high.
-def hand_sort_key(hand_bid):
-    hand = hand_bid[0]
-    court = {'T': '10', 'J': '11', 'Q': '12', 'K': '13', 'A': '14'}
-    key = ''
-    for c in hand:
-        if c.isdigit():
-            key += '0' + c
-        else:
-            key += (court.get(c, ''))
-    return key
+import re
 
 
 # Return a hand rank based on cards in the hand
-def analyse_hand(hand):
+def analyse_hand(hand, play_joker=False):
     three = False
     pair = high = 0
     hand_rank = ''
+    jokers = 0
     for _, type_group in itertools.groupby(sorted(hand)):
-        match len(list(type_group)):
+        gtype = list(type_group)
+        # if play_joker and J present, count them instead of assigning a rank. The other cards in
+        # the hand decide the rank, which is then modified by jokers, if any
+        if 'J' in gtype and play_joker:
+            jokers = len(gtype)
+            continue
+        match len(gtype):
             case 5:
                 hand_rank = 'five'
             case 4:
                 hand_rank = 'four'
             case 3:
-                three = True
                 hand_rank = 'three'
             case 2:
                 pair += 1
                 hand_rank = 'pair'
             case 1:
                 high += 1
-    if three and pair == 1:
+    if hand_rank == 'three' and pair == 1:
         hand_rank = 'fullhouse'
     if pair > 1:
         hand_rank = 'twopair'
-    if high == 5:
+    if high + jokers == 5:
         hand_rank = 'high'
+
+    # Adjust rank based on number of Jokers found
+    match jokers:
+        case 5:
+            hand_rank = 'five'
+        case 4:
+            hand_rank = 'four'
+        case 3:
+            match hand_rank:
+                case 'pair':
+                    hand_rank = 'five'
+                case 'high':
+                    hand_rank = 'four'
+        case 2:
+            match hand_rank:
+                case 'three':
+                    hand_rank = 'five'
+                case 'pair':
+                    hand_rank = 'four'
+                case 'high':
+                    hand_rank = 'three'
+        case 1:
+            match hand_rank:
+                case 'four':
+                    hand_rank = 'five'
+                case 'twopair':
+                    hand_rank = 'fullhouse'
+                case 'three':
+                    hand_rank = 'four'
+                case 'pair':
+                    hand_rank = 'three'
+                case 'high':
+                    hand_rank = 'pair'
     return hand_rank
 
 
-def part_one():
+def part_one_two(file, play_joker):
     game = {'high': [], 'pair': [], 'twopair': [], 'three': [], 'fullhouse': [], 'four': [], 'five': []}
 
     # Analyse each hand and push each hand, and it's bid into a list for rank
-    with open(os.path.join(os.path.dirname(__file__), "day07_data.txt"), "r", encoding="utf-8") as a_file:
+    with open(os.path.join(os.path.dirname(__file__), file), "r", encoding="utf-8") as a_file:
         for a_line in a_file:
             a_line = a_line.strip()
             # print(a_line)
             hand, bid = a_line.split()
-            hand_rank = analyse_hand(hand)
+            hand_rank = analyse_hand(hand, play_joker)
             s = game.get(hand_rank, [])
             s.append((hand, bid))
             game[hand_rank] = s
@@ -62,12 +88,29 @@ def part_one():
     # (e.g. pairs: 'KK7QA' > 'KK2QA' > 'TKTQA')
     winnings = 0
     inc = 1
-    for hands in game.values():
-        for hand, bid in sorted(hands, key=hand_sort_key):
+
+    # Provide a sort key for a card hand based on card letters representing values
+    # for the ten, court cards and aces high.
+    def hand_sort_key(hand_bid):
+        nonlocal play_joker
+        hand = hand_bid[0]
+        if play_joker:
+            court = {'J': '01', 'T': '10', 'Q': '12', 'K': '13', 'A': '14'}
+        else:
+            court = {'T': '10', 'J': '11', 'Q': '12', 'K': '13', 'A': '14'}
+        key = ''
+        for c in hand:
+            if c.isdigit():
+                key += '0' + c
+            else:
+                key += (court.get(c, ''))
+        return key
+
+    for rank, hand_rank in game.items():
+        for hand, bid in sorted(hand_rank, key=hand_sort_key):
+            print(re.sub('J', '\u001b[31mJ\u001b[0m', hand), rank, inc)
             winnings += int(bid) * inc
             inc += 1
 
     print(winnings)
     return winnings
-
-
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -1,12 +1,14 @@
-from aoc2023.day07.day07 import part_one, analyse_hand
+from aoc2023.day07.day07 import part_one_two, analyse_hand
 
 
-def test_part_one():
+def test_part_one_two():
     # day07_test.txt
-    # assert part_one() == 6440
+    # assert part_one_two('day07_test.txt', play_joker=False) == 6440
+    # assert part_one_two('day07_test.txt', play_joker=True) == 5905
 
     # day07_data.txt
-    assert part_one() == 252656917
+    # assert part_one_two('day07_data.txt', play_joker=False) == 252656917
+    assert part_one_two('day07_data.txt', play_joker=True) == 252656917
 
 
 def test_analyse_hand():
@@ -20,4 +22,10 @@ def test_analyse_hand():
     assert analyse_hand('8233A') == 'pair'
     assert analyse_hand('AAAAA') == 'five'
     assert analyse_hand('AKAAK') == 'fullhouse'
-    
+
+    assert analyse_hand('AJAAK', True) == 'four'
+    assert analyse_hand('JJJJJ', True) == 'five'
+    assert analyse_hand('23456', True) == 'high'
+    assert analyse_hand('KTJJT', True) == 'four'
+    assert analyse_hand('12JJ4', True) == 'three'
+    assert analyse_hand('11JJ4', True) == 'four'
